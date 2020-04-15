9b205570e544e7ec9817e9a8e5b9a249f07c1641
nguyen_dttn <contact.wilcity@gmail.com>
2020-04-15 08:49:18 +0700

    Improved Refresh Token

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace WilokeJWT\Controllers;
+
+class UserController
+{
+}
--- app/Helpers/Option.php
+++ app/Helpers/Option.php
@@ -11,7 +11,7 @@ class Option
     private static $userTokenKey = 'wilokejwt';
     private static $userRefreshTokenKey = 'wilokerefreshjwt';
     private static $aJWTOptions = [];
-
+    
     /**
      * @return array
      */
@@ -20,65 +20,68 @@ class Option
         if (!empty(self::$aJWTOptions)) {
             return self::$aJWTOptions;
         }
-
+        
         $aOptions = get_option(self::$optionKey);
         $aOptions = empty($aOptions) ? [] : $aOptions;
         if (empty($aOptions) && is_multisite()) {
             $aOptions = get_site_option(self::$optionKey);
         }
-
+        
         self::$aJWTOptions = wp_parse_args(
             $aOptions,
             [
                 'token_expiry' => 1,
-                'key' => uniqid(self::$optionKey . '_'),
-                'is_test' => 'no'
+                'key'          => uniqid(self::$optionKey.'_'),
+                'is_test'      => 'no'
             ]
         );
-
+        
         return self::$aJWTOptions;
     }
-
+    
     /**
      * @param $field
+     *
      * @return mixed|string
      */
     public static function getOptionField($field)
     {
         self::getJWTSettings();
-
+        
         return isset(self::$aJWTOptions[$field]) ? self::$aJWTOptions[$field] : '';
     }
-
+    
     public static function getAccessTokenKey()
     {
         $key = self::getRefreshTokenKey();
-        return 'access_token_' . $key;
+        
+        return 'access_token_'.$key;
     }
-
+    
     public static function getRefreshTokenKey()
     {
         $key = self::getOptionField('key');
+        
         return empty($key) ? 'wiloke_jwt' : $key;
     }
-
+    
     public static function getRefreshAccessTokenExp()
     {
         return strtotime('+1000 day');
     }
-
+    
     public static function getAccessTokenExp()
     {
         if (Option::getOptionField('is_test') === 'yes') {
             return strtotime('+1 minutes');
         }
-
+        
         $val = abs(Option::getOptionField('exp'));
         $val = empty($val) ? 1 : $val;
-
-        return strtotime('+' . $val . ' day');
+        
+        return strtotime('+'.$val.' day');
     }
-
+    
     /**
      * @param $val
      */
@@ -90,9 +93,10 @@ class Option
             update_option(self::$optionKey, $val);
         }
     }
-
+    
     /**
      * @param $userID
+     *
      * @return bool|int
      */
     private static function safeGetUserId($userID)
@@ -101,16 +105,17 @@ class Option
             if (!current_user_can('administrator')) {
                 return false;
             }
-
+            
             $userID = get_current_user_id();
         }
-
+        
         return $userID;
     }
-
+    
     /**
-     * @param $token
+     * @param        $token
      * @param string $userID
+     *
      * @return bool
      */
     public static function saveUserToken($token, $userID = '')
@@ -119,31 +124,38 @@ class Option
         if (empty($userID)) {
             return false;
         }
+        
         return update_user_meta($userID, self::$userTokenKey, $token);
     }
-
+    
     public static function saveUserRefreshToken($freshToken, $userID = '')
     {
         $userID = self::safeGetUserId($userID);
         if (empty($userID)) {
             return false;
         }
+        
         return update_user_meta($userID, self::$userRefreshTokenKey, $freshToken);
     }
-
+    
+    /**
+     * @param string $userID
+     *
+     * @return bool|mixed
+     */
     public static function getRefreshUserToken($userID = '')
     {
         $userID = self::safeGetUserId($userID);
         if (empty($userID)) {
             return false;
         }
-
+        
         return get_user_meta($userID, self::$userRefreshTokenKey, true);
     }
-
-
+    
     /**
      * @param string $userID
+     *
      * @return bool
      */
     public static function revokeRefreshToken($userID = '')
@@ -152,10 +164,10 @@ class Option
         if (empty($userID)) {
             return false;
         }
-
+        
         return delete_user_meta($userID, self::$userRefreshTokenKey);
     }
-
+    
     /**
      * @param string $userID
      *
@@ -167,12 +179,13 @@ class Option
         if (empty($userID)) {
             return false;
         }
-
+        
         return get_user_meta($userID, self::$userTokenKey, true);
     }
-
+    
     /**
      * @param string $userID
+     *
      * @return bool
      */
     public static function revokeAccessToken($userID = '')
@@ -181,7 +194,7 @@ class Option
         if (empty($userID)) {
             return false;
         }
-
+        
         return delete_user_meta($userID, self::$userTokenKey);
     }
 }
