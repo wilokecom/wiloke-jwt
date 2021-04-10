c60abd3dfc91d3a62cc2b927b3887d6e08ce6d86
Wiloke <contact.wilcity@gmail.com>
2021-04-10 22:50:41 +0700

    Nang cap phan LoginController.php
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- wiloke-jwt.php
+++ wiloke-jwt.php
@@ -17,19 +17,19 @@ use WilokeJWT\Controllers\PostTypeRegistry;
 use WilokeJWT\Controllers\UserProfileController;
 use WilokeJWT\DB\BlackListTokensTbl;
 
-require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
-define('WILOKE_JWT_API_NAMESPACE', 'wiloke-jwt');
-define('WILOKE_JWT_API_VERSION', 'v1');
-define('WILOKE_JWT_API', WILOKE_JWT_API_NAMESPACE . '/' . WILOKE_JWT_API_VERSION);
-define('WILOKE_JWT_VIEWS', plugin_dir_path(__FILE__) . 'app/Views/');
-define('INVALID_REFRESH_TOKEN', 'INVALID_REFRESH_TOKEN');
-define('WILOKE_JWT_VERSION', 1.0);
-define('WILOKE_JWT_URL', plugin_dir_url(__FILE__));
-define('WILOKE_JWT_PATH', plugin_dir_path(__FILE__));
-define('WILOKE_JWT_PREFIX', 'wiloke-jwt_');
+define( 'WILOKE_JWT_API_NAMESPACE', 'wiloke-jwt' );
+define( 'WILOKE_JWT_API_VERSION', 'v1' );
+define( 'WILOKE_JWT_API', WILOKE_JWT_API_NAMESPACE . '/' . WILOKE_JWT_API_VERSION );
+define( 'WILOKE_JWT_VIEWS', plugin_dir_path( __FILE__ ) . 'app/Views/' );
+define( 'INVALID_REFRESH_TOKEN', 'INVALID_REFRESH_TOKEN' );
+define( 'WILOKE_JWT_VERSION', 1.0 );
+define( 'WILOKE_JWT_URL', plugin_dir_url( __FILE__ ) );
+define( 'WILOKE_JWT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'WILOKE_JWT_PREFIX', 'wiloke-jwt_' );
 
-if (is_admin()) {
+if ( is_admin() ) {
 	new \WilokeJWT\Controllers\AdminMenuController();
 }
 
@@ -39,13 +39,13 @@ if (is_admin()) {
 global $oGenerateTokenController;
 
 $oGenerateTokenController = new \WilokeJWT\Controllers\GenerateTokenController();
-$oVerifyTokenController = new \WilokeJWT\Controllers\VerifyTokenController();
+$oVerifyTokenController   = new \WilokeJWT\Controllers\VerifyTokenController();
 //new UserProfileController;
 new PostTypeRegistry;
 new LoginController;
 
-register_activation_hook(__FILE__, function () {
-	do_action('wiloke_jwt_plugin_activated');
+register_activation_hook( __FILE__, function () {
+	do_action( 'wiloke_jwt_plugin_activated' );
 	new BlackListTokensTbl;
 	new \WilokeJWT\DB\PreLoginTbl();
-});
+} );
